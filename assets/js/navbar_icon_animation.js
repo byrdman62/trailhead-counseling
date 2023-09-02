a1a9eead5af482bb5d7d41e0b821cb4370af04cb
davbyr1 <david.byrd@jtv.com>
2023-09-01 21:10:29 -0400

    Made the mobile navbar transparent

--- assets/js/navbar_icon_animation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.navbar-hamburger').on('click', function () {
-      $('.animated-icon').addClass('open');
-    });
-    $('#navbarResponsive').on('hide.bs.collapse', function () {
-      $('.animated-icon').toggleClass('open');
-    });
-  });
